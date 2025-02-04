a38ad2ccdb4961cf7fe3f845a9b720796c68f9cc
MainakVerse <mainakchaudhuri671@gmail.com>
2025-02-04 15:32:48 +0530

    Added the files

---
+++ main.py
@@ -0,0 +1,31 @@
+import streamlit as st
+from web_functions import load_data
+
+from Tabs import diagnosis, home, result,  kc, talk2doc
+
+# Configure the app
+st.set_page_config(
+    page_title = 'Diabetes Prediction System',
+    page_icon = 'random',
+    layout = 'wide',
+    initial_sidebar_state = 'auto'
+)
+
+Tabs = {
+    "Home":home,
+    "Ask Queries":talk2doc,
+    "Diagnosis":diagnosis,
+    "Result":result,
+    # "Knowledge Center":kc
+}
+
+st.sidebar.title('Navigation')
+
+page = st.sidebar.radio("Page", list(Tabs.keys()))
+
+df, X, y = load_data()
+
+if page in ["Diagnosis"]:
+    Tabs[page].app(df, X, y)
+else:
+    Tabs[page].app()
